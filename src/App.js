f8abbd8eedab8febbdd8463486c93f71c85164c2
venkateshprasad598 <venkateshprasad598@gmail.com>
2021-08-17 13:11:40 +0530

    Add

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import logo from "./logo.svg";
 import "./App.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Header from "./components/Header";
 
-function App() {
+const App = () => {
   return (
     <Router>
       <div className="App">
         <Switch>
           <Route path="/checkout">
             <h1>Checkout</h1>
+          </Route>
+
+          <Route path="/login">
+            <h1>Login Page</h1>
           </Route>
 
           <Route className="/">
@@ -18,6 +22,6 @@ function App() {
       </div>
     </Router>
   );
-}
+};
 
 export default App;
