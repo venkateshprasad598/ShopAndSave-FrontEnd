936020d51b4efa975efd8c1ff189e3735063016f
venkateshprasad598 <venkateshprasad598@gmail.com>
2021-08-18 16:13:38 +0530

    Add

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -6,14 +6,48 @@ const Home = () => {
     <div className="home">
       <img
         className="home__img"
-        src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_FT_COVIDUPDATE_XSite_3000x1200_PV_en-GB._CB408004052_.jpg" //Banner Image
+        src="https://images-eu.ssl-images-amazon.com/images/G/31/img19/AmazonPay/Avatar/HeroPC_3000x1200_SVA._CB667240774_.jpg" //Banner Image
         alt=""
       />
 
       <div className="home__row">
         <Product
           id={123567}
-          title="My second project let's get started.what"
+          title="AmazonBasics 564 L Side-by-Side Door Refrigerator (Silver Steel Finish)"
+          image="https://images-na.ssl-images-amazon.com/images/I/71DuofqGR8L._SX679_.jpg"
+          price="48,999"
+          rating={5}
+        />
+
+        <Product
+          id={123567}
+          title="My second project is gonna be amazon, let's get started"
+          image="https://image01.oneplus.net/shop/202103/15/1-M00-22-09-rB8bwmBO1G-AO6MJAAChKvlT19g553.png"
+          price="80"
+          rating={5}
+        />
+
+        <Product
+          id={123567}
+          title="HP Pavilion Gaming 10th Gen Intel Core i5 Processor 15.6-inch(39.6 cm) FHD Gaming Laptop."
+          image="https://images-na.ssl-images-amazon.com/images/I/611VHOvjkES._SL1080_.jpg"
+          price="63,999"
+          rating={5}
+        />
+
+        <Product
+          id={123567}
+          title="Toshiba 7 Kg Fully Automatic Front Loading Washing Machine (‎TW-J80S2-IND, ‎White)"
+          image="https://images-na.ssl-images-amazon.com/images/I/61I42mnhfTS._SL1500_.jpg"
+          price="26,990"
+          rating={5}
+        />
+      </div>
+
+      <div className="home__row">
+        <Product
+          id={123567}
+          title="My second project let's get started."
           image="https://image01.oneplus.net/shop/202103/15/1-M00-22-09-rB8bwmBO1G-AO6MJAAChKvlT19g553.png"
           price="80"
           rating={5}
@@ -34,11 +68,13 @@ const Home = () => {
           price="80"
           rating={5}
         />
+      </div>
 
+      <div className="home__row">
         <Product
           id={123567}
-          title="My second project is gonna be amazon, let's get started"
-          image="https://image01.oneplus.net/shop/202103/15/1-M00-22-09-rB8bwmBO1G-AO6MJAAChKvlT19g553.png"
+          title="My second project let's get started."
+          image="https://images.samsung.com/is/image/samsung/in-rf9500kf-akg-rf28n9780sg-tl-frontblack-107074192?$720_576_PNG$"
           price="80"
           rating={5}
         />
