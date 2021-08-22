c71fe3044c54a77d8f4000f70eff80dadbaafcf9
venkateshprasad598 <venkateshprasad598@gmail.com>
2021-08-22 10:59:40 +0530

    Add

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Footer.css";
 import FooterInfo from "../FooterInfo/FooterInfo";
+import FooterBottom from "../FooterBottom/FooterBottom";
 
 const Footer = () => {
   return (
@@ -10,15 +11,46 @@ const Footer = () => {
         <div className="footer__backToTop">Back to top</div>
       </a>
 
-      <div className="footer__info">
-        <FooterInfo
-          title="Get to Know Us"
-          line1="About Us"
-          line2="Careers"
-          line3="Press Releases"
-          line4="Amazon Cares"
-          line5="Gift a Smile"
-        />
+      <div className="footer__socialInfo">
+        <div className="footer__Info">
+          <FooterInfo
+            title="Get to Know Us"
+            line1="About Us"
+            line2="Careers"
+            line3="Press Releases"
+            line4="Amazon Cares"
+            line5="Gift a Smile"
+          />
+          <FooterInfo
+            title="Connect with Us"
+            line1="Facebook"
+            line2="Twitter"
+            line3="instagram"
+          />
+          <FooterInfo
+            title="Mae Money with Us"
+            line1="Sell on Amazon"
+            line2="Sell under Amazon Accelerator"
+            line3="Amazon Global Selling"
+            line4="Become an Affiliate"
+            line5="Fulfilment by Amazon"
+            line6="Adverse Your Products"
+            line7="Amazon Pay on Merchants"
+          />
+          <FooterInfo
+            title="Let Us Help You"
+            line1="Covid-19 and Amazon"
+            line2="Returns Centre"
+            line3="100% Purchase Protection"
+            line4="Amazon App Download"
+            line5="Amazon Assistant Download"
+            line6="Your Account"
+            line7="Help"
+          />
+        </div>
+        <div className="footer__bottom">
+          <FooterBottom />
+        </div>
       </div>
     </div>
   );
